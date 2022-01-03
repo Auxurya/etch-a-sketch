36ed61a3643661747b1cf48ab932ecbef4c848df
Auxurya <cyber@auxurya.com>
2022-01-03 13:56:47 -0500

    Updated grid to take a user's width/length input & return a grid of 700x700 pixels regardless

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.getElementById('container');
+const userInput = prompt('Choose a grid width/length:')
 
-for (i = 0; i < 256; i++) {
-    let squareDiv = document.createElement('div');
-    squareDiv.classList.add("square");
-    container.appendChild(squareDiv);
+for (i = 0; i < userInput; i++) {
+    let rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    container.appendChild(rowDiv);
+    for (j = 0; j < userInput; j++) {
+        let squareDiv = document.createElement('div');
+        squareDiv.classList.add('square');
+        rowDiv.appendChild(squareDiv);
+    };
 };
 
 const squares = document.querySelectorAll('.square');
