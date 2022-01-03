2a3bdec880e8749105449369a363dcc4ed54ca1b
Auxurya <cyber@auxurya.com>
2022-01-03 14:32:57 -0500

    Centered the grid & added a reset button above it

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const container = document.getElementById('container');
-const userInput = prompt('Choose a grid width/length:')
-
-for (i = 0; i < userInput; i++) {
-    let rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    container.appendChild(rowDiv);
-    for (j = 0; j < userInput; j++) {
-        let squareDiv = document.createElement('div');
-        squareDiv.classList.add('square');
-        rowDiv.appendChild(squareDiv);
+const createGrid = () => {
+    container.innerHTML = '';
+    const userInput = prompt('Choose a grid width/length:')
+    for (i = 0; i < userInput; i++) {
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        container.appendChild(rowDiv);
+        for (j = 0; j < userInput; j++) {
+            let squareDiv = document.createElement('div');
+            squareDiv.classList.add('square');
+            rowDiv.appendChild(squareDiv);
+        };
+    };
+    const squares = document.querySelectorAll('.square');
+    for (i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('mouseenter', e => e.target.classList.add('mouseenter'));
     };
 };
 
-const squares = document.querySelectorAll('.square');
+createGrid();
 
-for (i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseenter', e => e.target.classList.add('mouseenter'));
-};
+const reset = document.getElementById('reset');
+reset.addEventListener('click', createGrid);
